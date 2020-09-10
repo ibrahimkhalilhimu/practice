c427103815ae0da5b186acd45c90b6bf3579a400
ibrahimkhalilhimu <ibrahimkhalilhemu1@gmail.com>
2020-09-10 21:25:58 +0600

    first commit

---
+++ forLoop.js
@@ -0,0 +1,11 @@
+var nums = [20,25,21,36,47,99];
+for (var i =0; i < nums.length; i++){
+    var element = nums[i];
+    console.log(element);
+}
+
+var friends = ['saiful','himu', 'zihad'];
+for(var i= 0; i< friends.length; i++){
+    var element = friends[i];
+    console.log(element)
+}
---
+++ function.js
@@ -0,0 +1,34 @@
+function name(){
+    console.log('my name is himu');
+   console.log('one');
+}
+name();
+
+function name1(){
+    console.log("himu");
+}
+name1();
+
+
+function dobble(num){
+    var result = num * 3
+    console.log(result);
+}
+dobble(3);
+
+function dobble1(num){
+    var complete = num *5
+    console.log(complete);
+}
+dobble1(5);
+
+
+function add (num){
+    var result = num * 2
+    return result;
+}
+var first = add (5);
+var second = add(20);
+var total = first + second
+console.log(total);
+
